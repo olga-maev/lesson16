15a15b922f99cad23df9d3874232b999244024d1
olga-maev <kurumoch0@gmail.com>
2022-08-06 23:05:15 +0400

    Add multi-player selection

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,24 +5,47 @@ public class Game {
 
     protected List<Player> playerList = new ArrayList<>();
 
+    public List<Player> getPlayerList() {
+        return playerList;
+    }
+
+    public List<Integer> strenghts = new ArrayList<>();
+
     public void register(Player player) {
         playerList.add(player);
     }
 
     public int round(String playerName1, String playerName2) {
-        if (playerList.size() < 2) {
-            throw new NoRegisteredExpection("Для продолжения игры необходимо зарегистироваться обоим игрокам");
+
+        strenghts.add(-1);
+        strenghts.add(-1);
+        for (int i = 0; i < playerList.size(); i++) {
+            if (playerList.get(i).name == playerName1) {
+                strenghts.set(0, playerList.get(i).getStrength());
+            }
+            if (playerList.get(i).name == playerName2) {
+                strenghts.set(1, playerList.get(i).getStrength());
+            }
         }
-        if (playerList.get(0).getStrength() > playerList.get(1).getStrength()) {
+        if (strenghts.get(0) == -1 && strenghts.get(1) == -1) {
+            throw new NoRegisteredExpection("Для продолжения игры необходимо зарегистрироваться обоим игрокам");
+        }
+        if (strenghts.get(0) == -1 && strenghts.get(1) != -1) {
+            throw new NoRegisteredExpection("Для продолжения игры первому игроку необходимо зарегистрироваться");
+        }
+        if (strenghts.get(0) != -1 && strenghts.get(1) == -1) {
+            throw new NoRegisteredExpection("Для продолжения игры второму игроку необходимо зарегистрироваться");
+        }
+        if (strenghts.get(0) > strenghts.get(1)) {
             return 1;
         } else {
-            if (playerList.get(0).getStrength() < playerList.get(1).getStrength()) {
+            if (strenghts.get(0) < strenghts.get(1)) {
                 return 2;
             } else {
                 return 0;
             }
         }
-
     }
+
 
 }
